7f353069239280df1622acd5ee2416ed2c3ff7eb
evgeniy-sibagatullin <evgeniy.sibagatullin@gmail.com>
2013-10-26 22:38:29 +0300

    Provide movement implementation and move rules to pawn.

--- src/game/set/ClassicChess.java
+++ src/game/set/ClassicChess.java
@@ -1,6 +1,7 @@
 package game.set;
 
 import enums.CellState;
+import enums.Direction;
 import enums.GameType;
 import enums.Side;
 import game.AbstractGame;
@@ -9,6 +10,7 @@ import piece.Piece;
 import piece.chess.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ClassicChess extends AbstractGame implements Chess {
@@ -16,6 +18,7 @@ public class ClassicChess extends AbstractGame implements Chess {
     private static final int FIELD_SIZE = 8;
     private Side activeSide;
     private boolean isPieceSelected;
+    private ModelCell selectedCell;
 
     public ClassicChess() {
         isPieceSelected = false;
@@ -24,7 +27,7 @@ public class ClassicChess extends AbstractGame implements Chess {
         initGameField();
         initPieces();
         addPiecesToGameField();
-        updateAllowedCells();
+        giveMoveToOtherSide();
     }
 
     private void initGameField() {
@@ -90,40 +93,108 @@ public class ClassicChess extends AbstractGame implements Chess {
 
     @Override
     public void clickCell(int row, int column) {
-        if (CellState.ALLOWED == getGameField()[row][column].getCellState()) {
-            if (isPieceSelected) {
-                isPieceSelected = false;
-                updateAllowedCells();
-            } else {
-                isPieceSelected = true;
+        ModelCell cell = getGameField()[row][column];
+        if (CellState.ALLOWED == cell.getCellState()) {
+            if (isPieceSelected && cell.getPiece() == null) {
+                movePiece(row, column);
                 activeSide = (activeSide == Side.WHITE) ? Side.BLACK : Side.WHITE;
-                updateAllowedCells();
-                getGameField()[row][column].setCellState(CellState.CHOOSE);
-                getGameField()[row][column].setChanged(true);
+                giveMoveToOtherSide();
+                isPieceSelected = false;
+
+            } else {
+                if (isPieceSelected) {
+                    activeSide = (activeSide == Side.WHITE) ? Side.WHITE : Side.BLACK;
+                    giveMoveToOtherSide();
+                } else {
+                    isPieceSelected = true;
+                }
+                updateAllowedCells(cell);
+                selectedCell = getGameField()[row][column];
+                selectedCell.setCellState(CellState.CHOOSE);
+                selectedCell.setChanged(true);
             }
         }
     }
 
-    private void updateAllowedCells() {
+    private void movePiece(int row, int column) {
+        getGameField()[row][column].setPiece(selectedCell.getPiece());
+        selectedCell.setPiece(null);
+    }
+
+    private void giveMoveToOtherSide() {
         for (int row = 0; row < FIELD_SIZE; row++) {
             for (int column = 0; column < FIELD_SIZE; column++) {
-                CellState cellState;
-                if (isPieceSelected) {
-                    cellState = (getGameField()[row][column].getPiece() == null) ?
-                            CellState.ALLOWED :
-                            CellState.DEFAULT;
-                } else {
-                    cellState = (getGameField()[row][column].getPiece() == null) ?
-                            CellState.DEFAULT :
-                            (getGameField()[row][column].getPiece().getSide() == activeSide) ?
-                                    CellState.ALLOWED :
-                                    CellState.DEFAULT;
-                }
+                CellState cellState = (getGameField()[row][column].getPiece() == null) ?
+                        CellState.DEFAULT :
+                        (getGameField()[row][column].getPiece().getSide() == activeSide) ?
+                                CellState.ALLOWED :
+                                CellState.DEFAULT;
                 if (getGameField()[row][column].getCellState() != cellState) {
                     getGameField()[row][column].setCellState(cellState);
                     getGameField()[row][column].setChanged(true);
                 }
             }
         }
+    }
+
+    private void updateAllowedCells(ModelCell cell) {
+        Piece piece = cell.getPiece();
+        if (piece instanceof Pawn) {
+            Direction direction = (piece.getSide() == Side.WHITE) ?
+                    Direction.NORTH :
+                    Direction.SOUTH;
+            int moveLength = 1;
+            if ((direction == Direction.NORTH && cell.getRow() == 6)
+                    || (direction == Direction.SOUTH && cell.getRow() == 1)) {
+                moveLength = 2;
+            }
+            updateAllowedCellsToMove(cell, new Direction[]{direction}, moveLength);
+        }
+    }
+
+    private void updateAllowedCellsToMove(ModelCell cell, Direction[] directions, int moveLength) {
+        for (Direction direction : directions) {
+            int deltaX = defineDeltaX(direction);
+            int deltaY = defineDeltaY(direction);
+            int row = cell.getRow();
+            int column = cell.getColumn();
+
+            for (int move = 0; move < moveLength; move++) {
+                row += deltaY;
+                column += deltaX;
+                if (row < FIELD_SIZE && column < FIELD_SIZE) {
+                    if (getGameField()[row][column].getPiece() == null) {
+                        getGameField()[row][column].setCellState(CellState.ALLOWED);
+                        getGameField()[row][column].setChanged(true);
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    private int defineDeltaX(Direction direction) {
+        Direction[] east = new Direction[]{Direction.NORTHEAST, Direction.EAST, Direction.SOUTHEAST};
+        Direction[] west = new Direction[]{Direction.NORTHWEST, Direction.WEST, Direction.SOUTHWEST};
+        int delta = 0;
+        if (Arrays.asList(east).contains(direction)) {
+            delta++;
+        } else if (Arrays.asList(west).contains(direction)) {
+            delta--;
+        }
+        return delta;
+    }
+
+    private int defineDeltaY(Direction direction) {
+        Direction[] south = new Direction[]{Direction.SOUTHWEST, Direction.SOUTH, Direction.SOUTHEAST};
+        Direction[] north = new Direction[]{Direction.NORTHWEST, Direction.NORTH, Direction.NORTHEAST};
+        int delta = 0;
+        if (Arrays.asList(south).contains(direction)) {
+            delta++;
+        } else if (Arrays.asList(north).contains(direction)) {
+            delta--;
+        }
+        return delta;
     }
 }
