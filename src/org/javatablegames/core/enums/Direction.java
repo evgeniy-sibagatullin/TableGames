39ce558876df5ffda1831107b925eaa43ae07197
evgeniy-sibagatullin <evgeniy.sibagatullin@gmail.com>
2013-12-22 20:47:33 +0200

    Add method to get direction via coordinate shift.

--- src/org/javatablegames/core/enums/Direction.java
+++ src/org/javatablegames/core/enums/Direction.java
@@ -11,12 +11,15 @@ public enum Direction {
     SOUTH(1, 0),
     SOUTHEAST(1, 1);
 
+    private static final Direction[] directions =
+            {NORTHWEST, NORTH, NORTHEAST, WEST, EAST, SOUTHWEST, SOUTH, SOUTHEAST};
+
     private final int deltaY;
     private final int deltaX;
 
-    private Direction(int deltaRow, int deltaColumn) {
-        this.deltaY = deltaRow;
-        this.deltaX = deltaColumn;
+    private Direction(int deltaY, int deltaX) {
+        this.deltaY = deltaY;
+        this.deltaX = deltaX;
     }
 
     public int getDeltaY() {
@@ -25,6 +28,15 @@ public enum Direction {
 
     public int getDeltaX() {
         return this.deltaX;
+    }
+
+    public static Direction getDirection(int deltaY, int deltaX) {
+        for (Direction direction : directions) {
+            if (direction.deltaY == Math.signum(deltaY) && direction.deltaX == Math.signum(deltaX)) {
+                return direction;
+            }
+        }
+        return null;
     }
 
 }
